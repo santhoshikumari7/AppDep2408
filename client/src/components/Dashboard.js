af061a6382530cb6f8afaff59642e08e135be362
santhoshi kumari <santhoshi77.kotyada@gmail.com>
2025-02-22 18:19:41 +0530

    done changes

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,82 +1,97 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import TopNavigation from './TopNavigation';
-import { useNavigate} from 'react-router-dom';
-
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import TopNavigation from "./TopNavigation";
+import { useNavigate } from "react-router-dom";
 
 function Dashboard() {
-   
   let navigate = useNavigate();
   let dispatch = useDispatch();
 
-  let storeObj = useSelector((store)=>{
+  let storeObj = useSelector((store) => {
     console.log(store);
     return store.loginReducer;
   });
 
-  let deleteProfile = async ()=>{
+  let deleteProfile = async () => {
+    let reqOptions = {
+      method: "DELETE",
+    };
 
-  let reqOptions = {
-    method:"DELETE"
-  }
+    let url = `/deleteProfile?email=${storeObj.loginDetails.email}`;
 
-  let url = `http://localhost:5000/deleteProfile?email=${storeObj.loginDetails.email}`
+    let JSONData = await fetch(url, reqOptions);
 
-  let JSONData = await fetch(url,reqOptions);
+    let JSOData = await JSONData.json();
 
-  let JSOData = await JSONData.json();
+    console.log(JSOData.msg);
+    alert(JSOData.msg);
 
-  console.log(JSOData.msg);
-  alert(JSOData.msg);
-
-  if(JSOData.status === "success") {
-    navigate("/");
-  }
-};
+    if (JSOData.status === "success") {
+      navigate("/");
+    }
+  };
 
   return (
     <div>
-      <TopNavigation/>
+      <TopNavigation />
       <h2>Dashboard</h2>
       <h3>
         {storeObj.loginDetails.firstName}
         {storeObj.loginDetails.lastName}
-        <button onClick={()=>{
-          deleteProfile();
-        }}
-        >Delete Profile</button>
+        <button
+          onClick={() => {
+            deleteProfile();
+          }}
+        >
+          Delete Profile
+        </button>
       </h3>
       <br></br>
-      <button onClick={()=>{
-
-        dispatch({type: "applyLeave",data: 1});
-
-      }}>Apply Leave</button>
-      <button onClick={()=>{
-        dispatch({type: "cancelLeave",data: 2});
-     }}
-      >Cancel Leave</button>
-      <button onClick={()=>{
-        dispatch({type: "addTask",data: 3});
-     }}
-      >Add Task</button>
-      <button onClick={()=>{
-        dispatch({type: "submitTask",data: 4});
-      }}
-      >Submit Task</button>
-      <button onClick={()=>{
-        dispatch({type: "hireEmployee",data: 5});
-      }}
-      >Hire Employee</button>
-      <button onClick={()=>{
-        dispatch({type: "fireEmployee",data: 6});
-      }}
-      >Fire Employee</button>
-      <img 
-      src={`http://localhost:5000/${storeObj.loginDetails.profilePic}`}
-      ></img>
+      <button
+        onClick={() => {
+          dispatch({ type: "applyLeave", data: 1 });
+        }}
+      >
+        Apply Leave
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "cancelLeave", data: 2 });
+        }}
+      >
+        Cancel Leave
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "addTask", data: 3 });
+        }}
+      >
+        Add Task
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "submitTask", data: 4 });
+        }}
+      >
+        Submit Task
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "hireEmployee", data: 5 });
+        }}
+      >
+        Hire Employee
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "fireEmployee", data: 6 });
+        }}
+      >
+        Fire Employee
+      </button>
+      <img src={`/${storeObj.loginDetails.profilePic}`}></img>
     </div>
   );
 }
 
-export default Dashboard
+export default Dashboard;
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,162 +1,159 @@
-import React, { useEffect } from 'react'
-import {Link ,useNavigate} from"react-router-dom";
-import  { useRef  } from 'react';
-import {useDispatch} from "react-redux";
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useRef } from "react";
+import { useDispatch } from "react-redux";
 import axios from "axios";
 
-
-
 function Login() {
+  let emailInaputRef = useRef();
+  let passwordInputRef = useRef();
 
- let emailInaputRef = useRef();
- let passwordInputRef = useRef();  
- 
- let navigate = useNavigate();
- let dispatch = useDispatch();
- 
- axios.defaults.baseURL = "http://localhost:5000";
+  let navigate = useNavigate();
+  let dispatch = useDispatch();
 
- useEffect(()=>{
-    if(localStorage.getItem("token")) {
+  axios.defaults.baseURL = "";
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
       //sendTokenToServer();
-      axios.defaults.headers.common['Authorization'] = localStorage.getItem("token");
-}
-}, []);
+      axios.defaults.headers.common["Authorization"] =
+        localStorage.getItem("token");
+    }
+  }, []);
 
-let sendTokenToServer = async ()=>{
-
-   let dataToSend = new FormData();
-   dataToSend.append("token",localStorage.getItem("token"));
-
-   let reqOptions = {
-    method: "POST",
-    body: dataToSend
-   }
-
-   let JSONData = await fetch("http://localhost:5000/validateToken",reqOptions);
-
-   let JSOData = await JSONData.json();
-   console.log(JSOData);
-
-    if (JSOData.status === "failure"){
-      alert(JSOData.msg);
-    }else{
-    localStorage.setItem("token",JSOData.data.token);
-    dispatch({ type: "login", data: JSOData.data })
-    navigate("/dashboard");
-     }
-    console.log(JSOData);
-
-};
-
-let onLogin = async ()=>{
+  let sendTokenToServer = async () => {
     let dataToSend = new FormData();
-     dataToSend.append("email",emailInaputRef.current.value);
-     dataToSend.append("password",passwordInputRef.current.value);
+    dataToSend.append("token", localStorage.getItem("token"));
 
     let reqOptions = {
-        method:"POST",
-        body:dataToSend
+      method: "POST",
+      body: dataToSend,
     };
 
-    let JSONData = await fetch("http://localhost:5000/login",reqOptions);
+    let JSONData = await fetch("/validateToken", reqOptions);
+
+    let JSOData = await JSONData.json();
+    console.log(JSOData);
+
+    if (JSOData.status === "failure") {
+      alert(JSOData.msg);
+    } else {
+      localStorage.setItem("token", JSOData.data.token);
+      dispatch({ type: "login", data: JSOData.data });
+      navigate("/dashboard");
+    }
+    console.log(JSOData);
+  };
+
+  let onLogin = async () => {
+    let dataToSend = new FormData();
+    dataToSend.append("email", emailInaputRef.current.value);
+    dataToSend.append("password", passwordInputRef.current.value);
+
+    let reqOptions = {
+      method: "POST",
+      body: dataToSend,
+    };
+
+    let JSONData = await fetch("/login", reqOptions);
 
     let JSOData = await JSONData.json();
 
-    if (JSOData.status === "failure"){
+    if (JSOData.status === "failure") {
       alert(JSOData.msg);
-    }else{
-    localStorage.setItem("token",JSOData.data.token);
-    dispatch({type:"login", data: JSOData.data})
-    navigate("/dashboard");
-     }
-    console.log(JSOData);
-};
-
- let onLogin2  = ()=>{
-   return async ()=>{ 
-     let dataToSend = new FormData();
-     dataToSend.append("email",emailInaputRef.current.value);
-     dataToSend.append("password",passwordInputRef.current.value);
-
-   let reqOptions = {
-       method:"POST",
-       body:dataToSend
-   };
-
-   let JSONData = await fetch("http://localhost:5000/login",reqOptions);
-
-   let JSOData = await JSONData.json();
-
-   if (JSOData.status === "failure"){
-     alert(JSOData.msg);
-   }else{
-   localStorage.setItem("token",JSOData.data.token);
-   dispatch({type:"login", data: JSOData.data})
-   navigate("/dashboard");
+    } else {
+      localStorage.setItem("token", JSOData.data.token);
+      dispatch({ type: "login", data: JSOData.data });
+      navigate("/dashboard");
     }
-   console.log(JSOData);
-}
+    console.log(JSOData);
+  };
 
- };
+  let onLogin2 = () => {
+    return async () => {
+      let dataToSend = new FormData();
+      dataToSend.append("email", emailInaputRef.current.value);
+      dataToSend.append("password", passwordInputRef.current.value);
 
- let onLogin3  = ()=>{
-  return async ()=>{ 
-    let dataToSend = new FormData();
-    dataToSend.append("email",emailInaputRef.current.value);
-    dataToSend.append("password",passwordInputRef.current.value);
+      let reqOptions = {
+        method: "POST",
+        body: dataToSend,
+      };
 
-  // let reqOptions = {
-  //     method:"POST",
-  //     body:dataToSend
-  // };
+      let JSONData = await fetch("/login", reqOptions);
 
-  let response = await axios.post("/login",dataToSend);
+      let JSOData = await JSONData.json();
 
-  console.log(response);
+      if (JSOData.status === "failure") {
+        alert(JSOData.msg);
+      } else {
+        localStorage.setItem("token", JSOData.data.token);
+        dispatch({ type: "login", data: JSOData.data });
+        navigate("/dashboard");
+      }
+      console.log(JSOData);
+    };
+  };
 
-  // let JSONData = await fetch("http://localhost:5000/login",reqOptions);
+  let onLogin3 = () => {
+    return async () => {
+      let dataToSend = new FormData();
+      dataToSend.append("email", emailInaputRef.current.value);
+      dataToSend.append("password", passwordInputRef.current.value);
 
-  // let JSOData = await JSONData.json();
+      // let reqOptions = {
+      //     method:"POST",
+      //     body:dataToSend
+      // };
 
-  if (response.data.status === "failure"){
-    alert(response.data.msg);
-  }else{
-  localStorage.setItem("token",response.data.data.token);
-  dispatch({type:"login", data: response.data.data})
-  navigate("/dashboard");
-   }
-  console.log(response.data);
-};
+      let response = await axios.post("/login", dataToSend);
 
-};
+      console.log(response);
+
+      // let JSONData = await fetch("/login",reqOptions);
+
+      // let JSOData = await JSONData.json();
+
+      if (response.data.status === "failure") {
+        alert(response.data.msg);
+      } else {
+        localStorage.setItem("token", response.data.data.token);
+        dispatch({ type: "login", data: response.data.data });
+        navigate("/dashboard");
+      }
+      console.log(response.data);
+    };
+  };
 
   return (
     <div className="App">
-    <form>
-      <h2>Login</h2>
+      <form>
+        <h2>Login</h2>
+        <div>
+          <label>Email</label>
+          <input ref={emailInaputRef}></input>
+        </div>
+        <div>
+          <label>Password</label>
+          <input ref={passwordInputRef}></input>
+        </div>
+        <div>
+          <button
+            type="button"
+            onClick={() => {
+              //onLogin ();
+              dispatch(onLogin3());
+            }}
+          >
+            Login
+          </button>
+        </div>
+      </form>
       <div>
-        <label>Email</label>
-        <input ref={emailInaputRef}></input>
-      </div>
-      <div>
-        <label>Password</label>
-        <input ref={passwordInputRef}></input>
-      </div>
-      <div>
-        <button type="button" onClick={()=>{
-            //onLogin ();
-            dispatch(onLogin3());
-        }}
-        >Login
-        </button>
-      </div>
-    </form>
-    <div>
         <Link to="/signup">Signup</Link>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
